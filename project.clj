30f71adf2eb4b0c1655697ee7d95ce32cf31ba13
Alex Petrov <alexp@coffeenco.de>
2013-10-28 22:26:07 +0100

    Update workshop lib

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [workshoplib "1.0.0"]
+                 [workshoplib "1.0.2"]
  		 [lein-light-nrepl "0.0.7"]]
   :aot [workshoplib.tdd
         defworkshop.java-interop]
